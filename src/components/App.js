c8ea1323062a4d7cf7dd292d29d2256dad80de2b
Harshita222 <kumariharsita501@gmail.com>
2024-07-05 21:19:58 +0530

    don

--- src/components/App.js
+++ src/components/App.js
@@ -7,21 +7,18 @@ const App = () => {
   const users = [
     { id: 1, name: 'Harshit Kumari', email: 'kumariharsita501@gmail.com', location: 'Siwan, Bihar' },
     { id: 2, name: 'John Doe', email: 'john.doe@example.com', location: 'New York, USA' },
-    // Add more users as needed
   ];
 
   return (
     <Router>
-      <div>
-        <Switch>
-          <Route exact path="/" render={() => <UserList users={users} />} />
-          <Route path="/user/:id" render={(props) => {
-            const userId = parseInt(props.match.params.id, 10);
-            const user = users.find(user => user.id === userId);
-            return <UserDetail user={user} />;
-          }} />
-        </Switch>
-      </div>
+      <Switch>
+        <Route exact path="/" render={() => <UserList users={users} />} />
+        <Route path="/user/:id" render={(props) => {
+          const userId = parseInt(props.match.params.id, 10);
+          const user = users.find(user => user.id === userId);
+          return <UserDetail user={user} />;
+        }} />
+      </Switch>
     </Router>
   );
 };
